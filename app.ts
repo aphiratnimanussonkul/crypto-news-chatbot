af721e4ba92b7dd6d2bff6587f8041bc93a4add1
Aphirat Nimanussonkul <Tata@odds.team>
2021-01-24 10:26:23 +0700

    call fuction intetval get news

--- app.ts
+++ app.ts
@@ -1,8 +1,11 @@
 import { appRoutes } from "./routes/app.routes";
+import { intervalGetNews5Min } from "./controller";
 
 var express = require("express");
 const app = express();
 const port = 3000;
+
+intervalGetNews5Min();
 
 app.use("/", appRoutes);
 
