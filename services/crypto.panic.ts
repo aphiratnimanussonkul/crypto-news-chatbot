18c7f43a6eb1239abefb7cc7ee82711e003e296b
Aphirat Nimanussonkul <Tata@odds.team>
2021-01-24 09:20:17 +0700

    refactor,
    move logic out of service, we will create controller and add logic on it

--- services/crypto.panic.ts
+++ services/crypto.panic.ts
@@ -1,12 +1,25 @@
 import { httpClient } from "../lib/http.client";
 import { NewsResponse } from "../models/news.response";
-import { News } from "../models/news";
-import { saveNewsToDB, getNewsById } from "./news";
+
 const config = {
   apiKey: "3a5d5effdc23555e8f1ad6bf6ab93fa3dfb7433b",
   baseUrl: "https://cryptopanic.com/api/v1/posts/",
 };
-export const getNews = async (currency: string): Promise<News> => {
+
+export const getAllNewsFromCryptoPanic = async (): Promise<NewsResponse> => {
+  let response = await httpClient.get(`${config.baseUrl}`, {
+    params: {
+      auth_token: config.apiKey,
+      kind: "news",
+    },
+  });
+  let result = response.data.results as NewsResponse;
+  return result;
+};
+
+export const getNewsFromCryptoPanic = async (
+  currency: string
+): Promise<NewsResponse> => {
   let response = await httpClient.get(`${config.baseUrl}`, {
     params: {
       auth_token: config.apiKey,
@@ -15,21 +28,5 @@ export const getNews = async (currency: string): Promise<News> => {
     },
   });
   let result = response.data.results as NewsResponse;
-  const lastNews: News = {
-    title: result[0].title,
-    url: result[0].url,
-    id: result[0].id,
-    currencies: result[0].currencies,
-  };
-  saveNewNews(lastNews);
-  return lastNews;
-};
-
-export const saveNewNews = async (news: News): Promise<News> => {
-  let newsFromDb = await getNewsById(news);
-  if (!newsFromDb) {
-    saveNewsToDB(news);
-    //call service to send news to user
-  }
-  return news;
+  return result;
 };
